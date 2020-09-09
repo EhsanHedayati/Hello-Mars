608351bdbce9a818139ced4d15e89f1dfa374c21
Ehsan <ehsan.hedayati83@gmail.com>
2020-09-09 12:56:37 +0430

    modify canary class

--- Canary.java
+++ Canary.java
@@ -6,6 +6,8 @@ class Canary{
     public static void main(String[] args) {
 
         System.out.println("Hello Canary Island");
+        System.out.println("Hello Canary Island");
+        System.out.println("Hello Canary Island");
 
     }
     
